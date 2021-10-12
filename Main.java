19664868d080a3d461a8dcb634fb4b744670728b
DriedSponge <jordan@driedsponge.net>
2021-10-11 23:17:48 -0700

    Finshed stuff for game

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-
+/**
+ * A:3 Simple game thing
+ * @author Jordan Tucker
+ *
+ * The ability to place bets and fold has not been added yet.
+ *
+ */
 class Main{
     public static void main(String[] args) {
         Game game = new Game();
