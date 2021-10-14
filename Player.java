d960804505bfb492161aa72a4f0855af3051616e
Jordan Tucker <jortuck0810@gmail.com>
2021-10-14 00:14:58 +0000

    Worked on betting

--- Player.java
+++ Player.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Player {
 
     private String name;
-    private int bet;
+    private int balance = 1000;
     private Card card;
 
     public Player(String name){
@@ -26,5 +28,25 @@ public class Player {
 
     public Card getCard() {
         return card;
+    }
+
+    public int getBet(){
+      Scanner userIn = new Scanner(System.in);
+      while(true){
+        System.out.print("Your balance is $"+this.balance+". How much would you like to bet? ");
+        if(userIn.hasNextInt()){
+          int bet = userIn.nextInt();
+          if(this.balance >= bet){
+            this.balance -= bet;
+            return bet;
+          }else{
+            System.out.println("Sorry, you don't have enough money for that bet.");
+            return 0;
+          }
+
+        }else{
+          System.out.println("Please input an integer.");
+        }
+      }
     }
 }
